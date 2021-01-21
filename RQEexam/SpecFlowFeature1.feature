791d21a7cc1529646e507e60bddf3eea4d09c2e8
Aya Metwalli <metwalliaya@gmail.com>
2021-01-21 17:14:17 +0100

    Add project files.

---
+++ RQEexam/SpecFlowFeature1.feature
@@ -0,0 +1,21 @@
+﻿Feature:  trigonometrische Funktionen
+	
+	Als Mathematiklehrer
+	möchte ich eine Taxhenrechner der  trigonometrische Funktionen berechnen kann.
+
+@mytag
+Scenario: sinus
+	Given the number is 3.1415
+	When I chose sinus
+	Then the result should be 0
+
+	Scenario: cosinus
+	Given the number is 3.1415
+	When I chose cosinus
+	Then the result should be -1
+
+
+	Scenario: tangens
+	Given the number is 3.1415
+	When I chose tangens
+	Then the result should be 0
---
+++ RQEexam/TrigonometrischeFunktionenSteps.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace RQEexam
+{
+    [Binding]
+    public class TrigonometrischeFunktionenSteps
+    {
+        int num, erg;
+
+        [Given(@"the number is (.*)")]
+        public void GivenTheNumberIs(Decimal p0)
+        {
+            num = (int)Math.PI;
+        }
+
+        [When(@"I chose sinus")]
+        public void WhenIChoseSinus()
+        {
+            erg = (int)Math.Sin(num);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = erg;
+            Assert.Equal(expected, actual);
+        }
+
+       
+
+        [When(@"I chose cosinus")]
+        public void WhenIChoseCosinus()
+        {
+            erg = (int)Math.Cos(num);
+        }
+
+        [When(@"I chose tangens")]
+        public void WhenIChoseTangens()
+        {
+            erg = (int)Math.Tan(num);
+        }
+
+      
+    }
+}
